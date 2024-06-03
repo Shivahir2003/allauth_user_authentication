61400aa50f9b65687f5491b6942b0a0fb783d29d
Shivahir2003 <shivahir0114@gmail.com>
2024-06-03 17:18:01 +0530

    docs: added docstring in view method

--- user_auth/dashboard/views.py
+++ user_auth/dashboard/views.py
@@ -4,4 +4,13 @@ from allauth.account.decorators import verified_email_required
 
 @verified_email_required
 def profile(request):
+    """ 
+       show user details after successful login
+       
+       Arguments:
+            request (HttpRequest)
+        
+        Returns:
+            In Get: render a page  
+    """
     return render(request,'dashboard/profile.html')
